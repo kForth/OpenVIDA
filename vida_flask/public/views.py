e649fad53adeb94b4989967dfa1a774c9b05a975
Kestin Goforth <kgoforth1503@gmail.com>
2025-01-28 15:48:54 -0500

    Start of part info page

--- vida_flask/public/views.py
+++ vida_flask/public/views.py
@@ -11,13 +11,29 @@ from flask import (
     render_template,
     request,
 )
-from vida_py import ServiceRepoSession
+from sqlalchemy import or_
+from sqlalchemy.orm import aliased
 from vida_py.basedata import BodyStyle, Engine, ModelYear, PartnerGroup
 from vida_py.basedata import Session as BaseDataSession
-from vida_py.basedata import SpecialVehicle, Steering, Transmission, VehicleModel
-from vida_py.diag import Session as DiagSession
-from vida_py.diag import get_valid_profiles_for_selected
-from vida_py.service import Document, DocumentProfile, Qualifier
+from vida_py.basedata import (
+    SpecialVehicle,
+    Steering,
+    Transmission,
+    VehicleModel,
+    VehicleProfile,
+)
+from vida_py.epc import (
+    AttachmentData,
+    CatalogueComponents,
+    ComponentAttachments,
+    ComponentConditions,
+    ComponentDescriptions,
+    Lexicon,
+    PartItems,
+)
+from vida_py.epc import Session as EpcSession
+from vida_py.images import LocalizedGraphics
+from vida_py.images import Session as ImageRepoSession
 
 from vida_flask.vida.api import get_document_html
 
@@ -69,8 +85,112 @@ def profile_select():
 
 
 @blueprint.route("/parts/")
-def parts():
+def part_list():
     return render_template("public/parts.html")
+
+
+@blueprint.route("/parts/<partnumber>")
+@blueprint.route("/parts/<partnumber>/<int:language>")
+def part_info(partnumber, language=15):
+    with BaseDataSession() as _basedata, EpcSession() as _epc, ImageRepoSession() as _images:
+        part, title = (
+            _epc.query(PartItems, Lexicon)
+            .outerjoin(Lexicon, Lexicon.DescriptionId == PartItems.DescriptionId)
+            .filter(PartItems.ItemNumber == partnumber, Lexicon.fkLanguage == language)
+            .one()
+        )
+        parent_lex = aliased(Lexicon)
+        desc_lex = aliased(Lexicon)
+        parent_desc = aliased(ComponentDescriptions)
+        comp_desc = aliased(ComponentDescriptions)
+        components = (  # profile, component, desc1, desc2, attachment
+            _epc.query(
+                ComponentConditions.fkProfile,
+                CatalogueComponents,
+                parent_lex,
+                desc_lex,
+                AttachmentData.Code,
+            )
+            .outerjoin(
+                ComponentConditions,
+                ComponentConditions.fkCatalogueComponent
+                == CatalogueComponents.ParentComponentId,
+            )
+            .outerjoin(
+                parent_desc,
+                parent_desc.fkCatalogueComponent == CatalogueComponents.ParentComponentId,
+            )
+            .outerjoin(
+                comp_desc,
+                comp_desc.fkCatalogueComponent == CatalogueComponents.Id,
+            )
+            .outerjoin(
+                parent_lex,
+                parent_lex.DescriptionId == parent_desc.DescriptionId,
+            )
+            .outerjoin(
+                desc_lex,
+                desc_lex.DescriptionId == comp_desc.DescriptionId,
+            )
+            .outerjoin(
+                ComponentAttachments,
+                ComponentAttachments.fkCatalogueComponent
+                == CatalogueComponents.ParentComponentId,
+            )
+            .outerjoin(
+                AttachmentData,
+                AttachmentData.Id == ComponentAttachments.fkAttachmentData,
+            )
+            .filter(
+                CatalogueComponents.fkPartItem == part.Id,
+                or_(
+                    parent_desc.DescriptionId == None,
+                    parent_lex.fkLanguage == language,
+                ),
+                or_(
+                    comp_desc.DescriptionId == None,
+                    desc_lex.fkLanguage == language,
+                ),
+            )
+            .order_by(CatalogueComponents.Id)
+            .all()
+        )
+        profiles = {
+            e.Id: e
+            for e in _basedata.query(VehicleProfile)
+            .filter(VehicleProfile.Id.in_([e[0] for e in components]))
+            .order_by(
+                VehicleProfile.fkPartnerGroup,
+                VehicleProfile.fkVehicleModel,
+                VehicleProfile.fkModelYear,
+                VehicleProfile.fkEngine,
+                VehicleProfile.fkTransmission,
+                VehicleProfile.fkSuspension,
+                VehicleProfile.fkSteering,
+                VehicleProfile.fkBrakeSystem,
+                VehicleProfile.fkBodyStyle,
+                VehicleProfile.fkSpecialVehicle,
+                VehicleProfile.fkNodeECU,
+                VehicleProfile.ChassisNoFrom,
+                VehicleProfile.ChassisNoTo,
+            )
+            .all()
+        }
+        graphics = {
+            g.fkGraphic: g.path
+            for g in _images.query(LocalizedGraphics)
+            .filter(LocalizedGraphics.fkGraphic.in_([e[-1] for e in components]))
+            .all()
+        }
+        components = [
+            (profiles[p], graphics.get(e[-1], None), *e) for p, *e in components
+        ]
+    return render_template(
+        "public/part.html",
+        part=part,
+        title=title,
+        components=components,
+    )
 
 
 @blueprint.route("/documents/")
