64fc9680c6759448aff4eb8e84de11181ce5065a
Kestin Goforth <kgoforth1503@gmail.com>
2026-07-16 09:44:11 -0400

    Add list of links for part

--- openvida/static/js/part.js
+++ openvida/static/js/part.js
@@ -1,1 +1,19 @@
-ko.applyBindings(new VidaBaseModel());
+class PartViewModel extends VidaBaseModel {
+    constructor() {
+        super();
+        var self = this;
+
+        self.component = ko.observable(ko.mapping.fromJS(PART));
+        self.sources = ko.observableArray([
+            { title: "VolvoCars.com", href: (item) => `https://usparts.volvocars.com/p/${item}.html` },
+            { title: "VolvoCars.ca", href: (item) => `https://parts.volvocars.ca/p/${item}.html` },
+            { title: "Toronto Volvo", href: (item) => `https://www.volvocarstorontoparts.ca/p/${item}.html` },
+            { title: "Hamilton Volvo", href: (item) => `https://parts.volvocarshamilton.com/p/${item}.html` },
+            { title: "FCP Euro", href: (item) => `https://www.fcpeuro.com/Volvo-parts?keywords=${item}` },
+        ]);
+
+        $.ajax()
+    }
+}
+var model = new PartViewModel();
+ko.applyBindings(model);
